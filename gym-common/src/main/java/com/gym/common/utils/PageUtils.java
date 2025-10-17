0a1ce489e3f384f2eb85c7624506e1cc9b3593d8
guoyixue <1010603372@qq.com>
2025-10-17 18:47:15 +0800

    chore: 兼容入参

--- gym-common/src/main/java/com/gym/common/utils/PageUtils.java
+++ gym-common/src/main/java/com/gym/common/utils/PageUtils.java
@@ -4,6 +4,7 @@ import com.github.pagehelper.PageHelper;
 import com.gym.common.core.page.PageDomain;
 import com.gym.common.core.page.TableSupport;
 import com.gym.common.utils.sql.SqlUtil;
+import com.gym.common.utils.StringUtils;
 
 /**
  * 分页工具类
@@ -20,8 +21,15 @@ public class PageUtils extends PageHelper
         PageDomain pageDomain = TableSupport.buildPageRequest();
         Integer pageNum = pageDomain.getPageNum();
         Integer pageSize = pageDomain.getPageSize();
-        String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
+        String orderBy = pageDomain.getOrderBy();
         Boolean reasonable = pageDomain.getReasonable();
+        
+        // 只有当orderBy不为空时才进行SQL验证
+        if (StringUtils.isNotEmpty(orderBy))
+        {
+            orderBy = SqlUtil.escapeOrderBySql(orderBy);
+        }
+        
         PageHelper.startPage(pageNum, pageSize, orderBy).setReasonable(reasonable);
     }
 
